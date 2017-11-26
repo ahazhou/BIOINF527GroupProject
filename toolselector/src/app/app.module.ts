b349902b6cfa2f15954deda1721c3bf035217d1b
Arnold Zhou <arnoldzhou@gmail.com>
2017-11-26 13:09:36 -0500

    Starting to do routing. Need to create a new routing module.

--- toolselector/src/app/app.module.ts
+++ toolselector/src/app/app.module.ts
@@ -1,15 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { QuestionsPageComponent } from './questions-page/questions-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: AppComponent
+  },
+  {
+    path: 'questions-page',
+    component: QuestionsPageComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    QuestionsPageComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }//<- debugging purposes only
+    ),
     NgbModule.forRoot(),
     BrowserModule
   ],
