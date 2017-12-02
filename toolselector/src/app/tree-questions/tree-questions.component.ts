9de4c848b0fd6ba8fbcf13e6fea4f3bfbc939c40
Arnold Zhou <arnoldzhou@gmail.com>
2017-12-02 11:39:03 -0500

    now all choices will have choice option "none of the above"

--- toolselector/src/app/tree-questions/tree-questions.component.ts
+++ toolselector/src/app/tree-questions/tree-questions.component.ts
@@ -9,29 +9,29 @@ export class TreeQuestionsComponent implements OnInit {
   public q0 = {
     name: "q0",
     title: "What type of RNA sequencing do you want to do?",
-    choices: ["Data Preprocessing", "Data Analysis", "Data Processing", "None of the above"]
+    choices: ["Data Preprocessing", "Data Analysis", "Data Processing"]
   }
   public q00 = {
     name: "q00",
     title: "What type of RNA data preprocessing do you want to do?",
-    choices: ["Experimental Design", "Base Calling", "Read Quality Control", "None of the above"],
+    choices: ["Experimental Design", "Base Calling", "Read Quality Control"],
     links: ["/tool", "/tool", "/tool"]
   }
   public q01 = {
     name: "q01",
     title: "What type of RNA data analysis do you want to do?",
-    choices: ["Exon Skipping Detection", "Time Couse Analysis", "Transposable Element Detection", "None of the above"],
+    choices: ["Exon Skipping Detection", "Time Couse Analysis", "Transposable Element Detection"],
     links: ["/tool", "/tool", "/tool"]
   }
   public q02 = {
     name: "q02",
     title: "What type of RNA data processing do you want to do?",
-    choices: ["Normalization", "Known Transcript Alignment", "Read Alignment", "None of the above"],
+    choices: ["Normalization", "Known Transcript Alignment", "Read Alignment"],
     links: ["/tool", "/tool", "/tool"]
   }
 
-  public changeQuestion = (index, name) =>{
-    if(index == this[name].choices.length - 1){
+  public changeQuestion = (index, name, isnone) =>{
+    if(isnone){
       window.location.href = "/not-available-yet"
     }
     else if(this[String(name+index)] == null){
